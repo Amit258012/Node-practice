b25daa59adafff4fd0b7c2b5e247f0c78deac13b
Amit258012 <amit258012@gmail.com>
2023-11-30 12:28:47 +0530

    creating simple API

--- index.js
+++ index.js
@@ -51,12 +51,37 @@ console.log("Will read file"); */
 // });
 
 // Topic (2.2): Routing
+// const server = http.createServer((req, res) => {
+// 	const pathName = req.url;
+// 	if (pathName === "/" || pathName === "/overview") {
+// 		res.end("This is overview Page");
+// 	} else if (pathName === "/product") {
+// 		res.end("List of all products");
+// 	} else {
+// 		res.writeHead(404, {
+// 			"Content-type": "text/html	",
+// 			"my-own-header": "I'm sending Meta data",
+// 		});
+// 		res.end(`<h1>page not found!</h1>`);
+// 	}
+// });
+
+// server.listen(8000, "127.0.0.1", () => {
+// 	console.log("Server is live at 8000");
+// });
+
+// Topic (2.3): Building Simple API
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const productData = JSON.parse(data);
 const server = http.createServer((req, res) => {
 	const pathName = req.url;
 	if (pathName === "/" || pathName === "/overview") {
 		res.end("This is overview Page");
 	} else if (pathName === "/product") {
 		res.end("List of all products");
+	} else if (pathName === "/api") {
+		res.writeHead(200, { "Content-type": "application/json" });
+		res.end(data);
 	} else {
 		res.writeHead(404, {
 			"Content-type": "text/html	",
@@ -70,4 +95,6 @@ server.listen(8000, "127.0.0.1", () => {
 	console.log("Server is live at 8000");
 });
 
-// Topic (2.3):
+// Chapter [3]: Node Farm
+
+// Topic (3.1): Building HTML Templates
