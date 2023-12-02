44844e1ed1156dd4f9303e619c2da6d245c4ea58
Amit258012 <amit258012@gmail.com>
2023-12-02 08:11:46 +0530

    dynamic , overview template rendering

--- index.js
+++ index.js
@@ -71,18 +71,83 @@ console.log("Will read file"); */
 // });
 
 // Topic (2.3): Building Simple API
+// const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+// const productData = JSON.parse(data);
+// const server = http.createServer((req, res) => {
+// 	const pathName = req.url;
+// 	if (pathName === "/" || pathName === "/overview") {
+// 		res.end("This is overview Page");
+// 	} else if (pathName === "/product") {
+// 		res.end("List of all products");
+// 	} else if (pathName === "/api") {
+// 		res.writeHead(200, { "Content-type": "application/json" });
+// 		res.end(data);
+// 	} else {
+// 		res.writeHead(404, {
+// 			"Content-type": "text/html	",
+// 			"my-own-header": "I'm sending Meta data",
+// 		});
+// 		res.end(`<h1>page not found!</h1>`);
+// 	}
+// });
+
+// server.listen(8000, "127.0.0.1", () => {
+// 	console.log("Server is live at 8000");
+// });
+
+// Chapter [3]: Node Farm
+
+// Topic (3.1): Building HTML Templates
+const replaceTemplate = (temp, product) => {
+	let output = temp.replaceAll("{%PRODUCTNAME%}", product.productName);
+	output = output.replaceAll("{%IMAGE%}", product.image);
+	output = output.replaceAll("{%PRICE%}", product.price);
+	output = output.replaceAll("{%FROM%}", product.from);
+	output = output.replaceAll("{%NUTRIENTS%}", product.nutrients);
+	output = output.replaceAll("{%QUANTITY%}", product.quantity);
+	output = output.replaceAll("{%DESCRIPTION%}", product.description);
+	output = output.replaceAll("{%ID%}", product.id);
+	if (!product.organic) {
+		output = output.replaceAll("{%NOT_ORGANIC%}", "not-organic");
+	}
+	return output;
+};
+const tempOverview = fs.readFileSync(
+	`${__dirname}/templates/overview.html`,
+	"utf-8"
+);
+const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`, "utf-8");
+const tempProduct = fs.readFileSync(
+	`${__dirname}/templates/product.html`,
+	"utf-8"
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const productData = JSON.parse(data);
 const server = http.createServer((req, res) => {
 	const pathName = req.url;
+
+	// overview Page
 	if (pathName === "/" || pathName === "/overview") {
-		res.end("This is overview Page");
-	} else if (pathName === "/product") {
+		res.writeHead(200, { "content-type": "text/html" });
+
+		const cardsHtml = productData
+			.map((el) => replaceTemplate(tempCard, el))
+			.join("");
+		const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+		res.end(output);
+	}
+
+	// product page
+	else if (pathName === "/product") {
 		res.end("List of all products");
-	} else if (pathName === "/api") {
+	}
+	// API page
+	else if (pathName === "/api") {
 		res.writeHead(200, { "Content-type": "application/json" });
 		res.end(data);
-	} else {
+	}
+	// Page Not Found
+	else {
 		res.writeHead(404, {
 			"Content-type": "text/html	",
 			"my-own-header": "I'm sending Meta data",
@@ -94,7 +159,3 @@ const server = http.createServer((req, res) => {
 server.listen(8000, "127.0.0.1", () => {
 	console.log("Server is live at 8000");
 });
-
-// Chapter [3]: Node Farm
-
-// Topic (3.1): Building HTML Templates
