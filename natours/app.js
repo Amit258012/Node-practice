8f53c0b94dfab8ae86814d67b635811224f01bc5
Amit258012 <amit258012@gmail.com>
2023-12-03 10:00:56 +0530

    handling get request

--- natours/app.js
+++ natours/app.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
-app.get("/", (req, res) => {
-	res.status(400).json({ message: "Hello World!", app: "Natour" });
-});
+// app.get("/", (req, res) => {
+// 	res.status(400).json({ message: "Hello World!", app: "Natour" });
+// });
 
-app.post("/", (req, res) => {
-	res.send("Send your message to this endpoint");
+// app.post("/", (req, res) => {
+// 	res.send("Send your message to this endpoint");
+// });
+
+// Topic: Handle Get requests
+
+const tours = JSON.parse(
+	fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+	res.status(200).json({
+		status: "success",
+		results: tours.length,
+		data: {
+			tours,
+		},
+	});
 });
 
 const port = 8000;
