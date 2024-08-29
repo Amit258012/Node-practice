4856ce9ad124ec5b60cd8ffc98a455d710517fee
Amit258012 <amit258012@gmail.com>
2024-08-29 14:13:42 +0530

    error handling middleware

--- natours/app.js
+++ natours/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
@@ -31,5 +33,22 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use("/api/v1/tours", tourRouter); //Middleware
 app.use("/api/v1/users", userRouter); //Middleware
+
+//  Handle wrong urls
+app.all("*", (req, res, next) => {
+	// res.status(404).json({
+	// 	status: "fail",
+	// 	message: ` Can't find ${req.originalUrl} on the server! `,
+	// });
+
+	// const err = new Error(` Can't find ${req.originalUrl} on the server! `);
+	// err.status = "fail";
+	// err.statusCode = 404;
+
+	next(new AppError(` Can't find ${req.originalUrl} on the server! `, 400));
+});
+
+// Error handling Middleware
+app.use(globalErrorHandler);
 
 module.exports = app;
--- natours/routes/tourRoutes.js
+++ natours/routes/tourRoutes.js
@@ -8,7 +8,7 @@ const {
 	deleteTour,
 	getTourStats,
 	getMonthlyPlan,
-} = require("../contollers/tourController");
+} = require("../controllers/tourController");
 const router = express.Router();
 
 // [ ]: Tour Route Functions
